36d428e5d72451b4279acd7a3ed287e5be4003c4
mvrlrd <mvrlrd@yandex.ru>
2024-07-22 12:33:07 +0300

    apply animations

--- app/src/main/java/com/example/cupcake/CupcakesApp.kt
+++ app/src/main/java/com/example/cupcake/CupcakesApp.kt
@@ -3,6 +3,10 @@ package com.example.cupcake
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,7 +25,24 @@ fun CupcakesApp(onToggleTheme: () -> Unit, darkTheme: Boolean, context: Context)
             viewModel.resetOrder()
             navController.navigate(Routes.StartScreen.route)
         }
-        NavHost(navController = navController, startDestination = Routes.StartScreen.route){
+        NavHost(navController = navController,
+            startDestination = Routes.StartScreen.route,
+            enterTransition = {
+                slideInHorizontally(initialOffsetX = { 1000 }) + fadeIn()
+            },
+            exitTransition = {
+                slideOutHorizontally(targetOffsetX = { -1000 }) + fadeOut()
+            },
+            popEnterTransition = {
+                slideInHorizontally(initialOffsetX = { -1000 }) + fadeIn()
+            },
+            popExitTransition = {
+                slideOutHorizontally(targetOffsetX = { 1000 }) + fadeOut()
+            },
+
+
+
+            ){
             composable(Routes.StartScreen.route){
                 StartScreen(viewModel = viewModel, goToFlavorScreen = {navController.navigate(Routes.FlavorScreen.route)})
             }
